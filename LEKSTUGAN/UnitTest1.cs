f7211aa2d70a8536f17d1e96e9c2371ff3925a94
victor <victor@DESKTOP-DEI1BKE>
2021-08-24 14:48:31 +0200

    Add project files.

---
+++ LEKSTUGAN/UnitTest1.cs
@@ -0,0 +1,75 @@
+using System;
+using Xunit;
+
+namespace LEKSTUGAN
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test0()
+        {
+            Assert.Equal(4, 4);
+            Assert.Equal(4,4);
+            Assert.True(true);
+            Assert.False(false);
+        }
+
+        [Fact]
+
+        public void Testvar()
+
+        {
+            string name;
+            bool lever = false;
+
+            if (lever)
+            {
+                name = "Mr Smith";
+            }
+            else
+            {
+                name = "Björn";
+            }
+        }
+
+        [Fact]
+
+        public void Test1()
+        {
+            Assert.Equal(5,2+3);
+        }
+
+        [Fact]
+
+        public void Test2()
+        {
+            Assert.True("Jag har fyllt" + 29 == "Jag har fyllt 29");
+        }
+
+        [Fact]
+
+        public void Test3()
+        {
+            int bigNumber = 2147483647;
+            int biggerNumber = bigNumber + 1;
+            Assert.True(biggerNumber > bigNumber);
+        }
+
+        public void Test4()
+        {
+            string drink;
+            int Age = 20;
+
+            if (Age > 18)
+            {
+                drink = "Beer";
+            }
+            else
+            {
+                drink = "Cola";
+            }
+
+        }
+
+    }
+}
